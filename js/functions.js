34b065313d1f2f5c592b56e2173cc002fdb96b99
Elina <elina50505051@yandex.ru>
2023-06-17 16:35:54 +0300

    Перенесла старые функции в functions.js

---
+++ js/functions.js
@@ -0,0 +1,33 @@
+function isValid (checkedString, maxLength) {
+  /*Функция для проверки длины строки*/
+  return checkedString.length <= maxLength;
+}
+
+function isPalindrome (checkedString) {
+  /*Функция для проверки, является ли строка палиндромом*/
+  checkedString = checkedString.replaceAll(' ', '').toLowerCase();
+  let reversedString = '';
+  for (let i = checkedString.length - 1; i >= 0; i--) {
+    reversedString += checkedString[i];
+  }
+  return checkedString === reversedString;
+}
+
+function parseNumbers (checkedInput) {
+  /*Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9
+  и возвращает их в виде целого положительного числа.*/
+  let formattedInput = '';
+  if (typeof checkedInput === 'number') {
+    checkedInput = checkedInput.toString();
+  }
+  for (let i = 0; i < checkedInput.length; i++) {
+    if (!Number.isNaN(parseInt(checkedInput[i], 10))) {
+      formattedInput += Math.abs(parseInt(checkedInput[i], 10));
+    }
+  }
+  return parseInt(formattedInput, 10);
+}
+
+isValid('Строка', 6);
+isPalindrome('топот');
+parseNumbers('ECMAScript 2022');
