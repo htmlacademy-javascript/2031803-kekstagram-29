c10ea86c967208b2d5d14f87581d3ae76274714d
Elina <elina50505051@yandex.ru>
2023-06-17 19:00:42 +0300

    Добавлены тесты

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,10 @@
+import * as assert from 'assert';
+
 const isValid = (checkedString, maxLength) => checkedString.length <= maxLength;
+
+assert.equal(isValid('Проверяемая строка', 20), true);
+assert.equal(isValid('Проверяемая строка', 18), true);
+assert.equal(isValid('Длинная проверяемая строка', 10), false);
 
 const isPalindrome = (checkedString) => {
   /*Функция для проверки, является ли строка палиндромом*/
@@ -9,6 +15,10 @@ const isPalindrome = (checkedString) => {
   }
   return checkedString === reversedString;
 };
+
+assert.equal(isPalindrome('топот'), true);
+assert.equal(isPalindrome('ДовОд'), true);
+assert.equal(isPalindrome('Кекс'), false);
 
 const extractNumbers = (checkedInput) => {
   /*Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9
@@ -25,9 +35,10 @@ const extractNumbers = (checkedInput) => {
   return parseInt(formattedInput, 10);
 };
 
-isValid('Строка', 6);
-isPalindrome('топот');
-extractNumbers('ECMAScript 2022');
+assert.equal(extractNumbers('2023 год'), 2023);
+assert.equal(extractNumbers('ECMAScript 2022'), 2022);
+assert.equal(extractNumbers('1 кефир, 0.5 батона'), 105);
+assert.equal(extractNumbers('агент 007'), 7);
 
 
 function isMeetingAvailable (beginningOfWorkday, endOfWorkday, beginningOfMeeting, meetingDuration) {
@@ -35,9 +46,11 @@ function isMeetingAvailable (beginningOfWorkday, endOfWorkday, beginningOfMeetin
     arguments[i] = parseFloat(arguments[i].replace(':', '.'));
     arguments[i] = Math.trunc(arguments[i]) * 60 + arguments[i] % 1 * 100;
   }
-  console.log(beginningOfWorkday);
   return (arguments[2] >= arguments[0] && arguments[2] + meetingDuration <= arguments[1]);
 };
 
-
-console.log(isMeetingAvailable('8:0', '10:0', '8:0', 120)); // true
+assert.equal(isMeetingAvailable('08:00', '17:30', '14:00', 90), true);
+assert.equal(isMeetingAvailable('8:0', '10:0', '8:0', 120), true);
+assert.equal(isMeetingAvailable('08:00', '14:30', '14:00', 90), false);
+assert.equal(isMeetingAvailable('14:00', '17:30', '08:0', 90), false);
+assert.equal(isMeetingAvailable('8:00', '17:30', '08:00', 900), false);
