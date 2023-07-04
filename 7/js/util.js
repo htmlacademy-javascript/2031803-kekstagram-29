857bc9dbf7302db5a7156c18ed1949f989eae024
Keks <keks@htmlacademy.ru>
2023-07-04 15:30:44 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/util.js
@@ -0,0 +1,39 @@
+import {closeBigPicture} from './fullscreen-mode.js';
+
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const createUniqueId = (min, max) => {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const onPopupEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeBigPicture();
+  }
+};
+
+const getSelectedPhotoIndex = (array, evt) => array.findIndex((obj) => obj.url === evt.target.getAttribute('src'));
+
+export {getRandomInteger, createUniqueId, getRandomArrayElement, isEscapeKey, onPopupEscKeydown, getSelectedPhotoIndex};
